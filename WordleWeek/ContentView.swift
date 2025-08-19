f93c35493f1fb14436ac2ffb1ddbc0ffaa88c07a
Sumukh Paspuleti <sumukhpaspuleti@Sumukhs-Mac.local>
2025-08-19 08:24:02 -0700

    Initial commit

---
+++ WordleWeek/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WordleWeek
+//
+//  Created by Sumukh Paspuleti on 8/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ WordleWeek/WordleWeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleWeekApp.swift
+//  WordleWeek
+//
+//  Created by Sumukh Paspuleti on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct WordleWeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
