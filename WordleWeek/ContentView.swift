059d7ad96c42b26a65ee1f3c80bb05fca354943d
SumukhP-dev <sumukh.paspuleti@gmail.com>
2025-08-19 16:06:20 -0700

    Simplify hello world page

--- WordleWeek/ContentView.swift
+++ WordleWeek/ContentView.swift
@@ -9,12 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Hello, World!")
     }
 }
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
