ddfd361cd7452b640602fc986a11abb0d7c22409
SumukhP-dev <sumukh.paspuleti@gmail.com>
2025-08-19 17:39:05 -0700

    Add 6 by 5 letter grid

--- WordleWeek/ContentView.swift
+++ WordleWeek/ContentView.swift
@@ -7,16 +7,54 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LetterTile: View {
+    let letter: String
+    let backgroundColor: Color
+    
     var body: some View {
-        Text("Hello, World!")
+        Text(letter.isEmpty ? " " : letter)
+            .font(.title)
+            .fontWeight(.bold)
+            .foregroundColor(.black)
+            .frame(width: 60, height: 60)
+            .background(letter.isEmpty ? Color.gray.opacity(0.3) : backgroundColor)
+            .border(Color.gray, width: 2)
+            .cornerRadius(5)
     }
 }
 
-#if DEBUG
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+    @State var gridLetters: [[String]] = [
+        ["W", "O", "R", "L", "D"],
+        ["", "", "", "", ""],
+        ["", "", "", "", ""],
+        ["", "", "", "", ""],
+        ["", "", "", "", ""],
+        ["", "", "", "", ""]
+    ]
+    
+    @State var gridColors: [[Color]] = [
+        [.green, .yellow, .gray, .green, .gray],
+        [.clear, .clear, .clear, .clear, .clear],
+        [.clear, .clear, .clear, .clear, .clear],
+        [.clear, .clear, .clear, .clear, .clear],
+        [.clear, .clear, .clear, .clear, .clear],
+        [.clear, .clear, .clear, .clear, .clear],
+    ]
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            ForEach(0..<6, id: \.self) { row in
+                HStack(spacing: 8) {
+                    ForEach(0..<5, id: \.self)  { col in
+                        LetterTile(
+                            letter: gridLetters[row][col],
+                            backgroundColor: gridColors[row][col]
+                        )
+                    }
+                }
+            }
+        }
+        .padding()
     }
 }
-#endif
